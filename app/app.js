ac2d20c4ca9b2b7f235afc8e6954ad0557bcf11a
OldGordon <grindersan@gmail.com>
2016-02-24 13:16:35 +0100

    Empieza el código Min max

---
+++ app/app.js
@@ -0,0 +1,43 @@
+/*jslint browser: true*/
+/*jslint node: true*/
+/*jshint strict: false*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+	'use strict';
+	function MinMax(game, control) {
+		if (game.over) {
+			control += 1;
+			var scores = [],
+				moves = [];
+		}
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
