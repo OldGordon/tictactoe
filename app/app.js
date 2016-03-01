0f21f971eaf03acf02866ce5823b3e889b391d4b
OldGordon <grindersan@gmail.com>
2016-03-01 10:15:33 +0100

    Hola

--- app/app.js
+++ app/app.js
@@ -7,18 +7,21 @@ $(document).ready(function () {
 	'use strict';
 
 	var Game = function () {
-		this.player1 = "X";
-		this.player2 = "O";
+		this.cpuMax = 2;
+		this.cpuMin = 1;
+		this.board = [];
 
 	};
-	Game.prototype.checkBoard = function () {
-		var board = [];
+
+	Game.prototype.getBoard = function () {
+
 		for (var i = 0; i < 9 ; i++) {
-			board[i] = $("#item" + (i + 1)).html();
+			this.board[i] = $("#item" + (i + 1)).html();
 		}
-		return board;
+		return this.board;
 
 	};
+
 	//This checks if we have the same token in  vertical , horizontal and diagonal lines
     Game.prototype.checkWin = function (player, board) {
 		if (
@@ -40,10 +43,10 @@ $(document).ready(function () {
 	//this function checks if the board is complete return false
 	Game.prototype.checkTie = function () {
 
-		var board = this.checkBoard(),
-			arrLengh = board.length;
+		var board = this.getBoard();
+
 		//if we found an empty cell the game is not over
-		for (var i = 0; i < arrLengh; i++) {
+		for (var i = 0; i < 9; i++) {
 			if (board[i] === ""){
 				return false;
 			}
@@ -51,18 +54,44 @@ $(document).ready(function () {
 		return  true;
 	};
 	//this provide to the game logic where CPU can do a valid move
-	Game.prototype.emptyCells = function (i, maxmin, board) {
+	Game.prototype.cpuPlay = function (i, maxMin, board) {
 		//we need a new board
-		var newBoard = this.checkBoard();
+		var newBoard = this.getBoard();
 		if(newBoard[i] === ""){
-			newBoard[i] = maxmin;
+			newBoard[i] = maxMin;
 			return newBoard;
 		} else {
 			return false;
 		}
+	};
+
+	Game.prototype.emptyCell = function (board) {
+		var bestCell = -10,
+			rank = 0;
+		for (var i = 0; i < 9; i++) {
+			var newBoard = this.cpuPlay(i, rank, board);
+			if(newBoard){
+				var netxValue = this.maxmin(newBoard);
+				if (netxValue < bestCell) {
+					netxValue = bestCell;
+					rank = i;
+
+				}
+			}
+		}
+		return rank;
+	};
+	Game.prototype.maxMin = function(board) {
+		if(this.checkWin(this.cpuMax, board)){
+			return 1;
+		} else if (this.checkWin(this.cpuMin, board)) {
+			return -1;
+		} else if (this.checkTie(board) ){
+			return 0;
+		} else {
 
 
-
+		}
 	};
 });
 
