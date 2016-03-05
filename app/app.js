3779203bbbe93bab9112eb4f1cf2aad17763aefe
OldGordon <grindersan@gmail.com>
2016-03-05 20:45:32 +0100

    Terminado , funcionando a la espera de pasarlo a revealing prototype pattern

--- app/app.js
+++ app/app.js
@@ -3,7 +3,6 @@
 /*jshint strict: false*/
 /*global $, jQuery, alert*/
 
-
 	var player = 1,
 		cpu = 2,
 		playing = 0,
@@ -12,6 +11,7 @@
 
 	//constructor for the game board
 	var Board = function () {
+
 		//we keep here 1,2 or 0 if the cell is empty
 		this.board = [];
 		//to write the "X" or "O" symbols
@@ -24,70 +24,83 @@
 
 	};
 	//this reset to 0 all the board in the board.Return nothing
-	Board.prototype.reset = function() {
-		this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-	};
-	//checks if a cell is empty.Return Bool
-	Board.prototype.isEmptyCell = function(val) {
-		return (this.board[val] === 0 );
-	};
-	//write a cell with the value passed
-	Board.prototype.writeCell = function(num, cell) {
-		this.board[cell] = num;
-		//console.log(cell , num);
-	};
-	//This checks if we have the same token in  vertical , horizontal and diagonal lines
-    Board.prototype.checkWin = function(player) {
-		//console.log(this.cells);
-		if (
-			(this.board[0] === player && this.board[1] === player && this.board[2] === player) ||
-			(this.board[3] === player && this.board[4] === player && this.board[5] === player) ||
-			(this.board[6] === player && this.board[7] === player && this.board[8] === player) ||
-			//horizontal lines
-			(this.board[0] === player && this.board[3] === player && this.board[6] === player) ||
-			(this.board[1] === player && this.board[4] === player && this.board[7] === player) ||
-			(this.board[2] === player && this.board[5] === player && this.board[8] === player) ||
-			//vertical lines
-			(this.board[0] === player && this.board[4] === player && this.board[8] === player) ||
-			(this.board[2] === player && this.board[4] === player && this.board[6] === player)
-			//diagonal lines
-		) {
-			//console.log(board[6]);
-			return true;
-		} else {
-			return false;
-		}
+	Board.prototype = function() {
 
-	};
+		var reset = function() {
+		this.board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+		},
+
+	//checks if a cell is empty.Return Bool
+		isEmptyCell = function(val) {
+			return (this.board[val] === 0 );
+		},
+	//write a cell with the value passed
+		writeCell = function(num, cell) {
+			this.board[cell] = num;
+
+		},
+	//This checks if we have the same token in  vertical , horizontal and diagonal lines
+    	checkWin = function(player) {
+		//console.log(this.cells);
+			if (
+				(this.board[0] === player && this.board[1] === player && this.board[2] === player) ||
+				(this.board[3] === player && this.board[4] === player && this.board[5] === player) ||
+				(this.board[6] === player && this.board[7] === player && this.board[8] === player) ||
+				//horizontal lines
+				(this.board[0] === player && this.board[3] === player && this.board[6] === player) ||
+				(this.board[1] === player && this.board[4] === player && this.board[7] === player) ||
+				(this.board[2] === player && this.board[5] === player && this.board[8] === player) ||
+				//vertical lines
+				(this.board[0] === player && this.board[4] === player && this.board[8] === player) ||
+				(this.board[2] === player && this.board[4] === player && this.board[6] === player)
+				//diagonal lines
+			) {
+				//console.log(board[6]);
+				return true;
+			} else {
+				return false;
+			}
+
+		},
 	//this function checks if the board is complete return true
-	Board.prototype.checkTie = function () {
+		checkTie = function () {
 
 		//if we found an empty cell the game is not over
-		for (var i = 0; i < 9; i++) {
-			//console.log(this.board[i]);
-			if (this.board[i] == 0) {
-				return true;
+			for (var i = 0; i < 9; i++) {
+				//console.log(this.board[i]);
+				if (this.board[i] === 0) {
+					return true;
+				}
 			}
-		}
-		return false;
-	};
+			return false;
+	},
 
 
 	//we draw the X or O corresponding to the player or cpu turn
-	Board.prototype.drawToken = function () {
-		for (var i = 0; i < 9; i++) {
-			//if the square is empty
-			if(this.board[i] == 0) {
-				this.cells[i].innerHTML = "";
-			} else {
-				if (this.board[i] === player){
-					this.cells[i].innerHTML = "X";
+		drawToken = function () {
+			for (var i = 0; i < 9; i++) {
+				//if the square is empty
+				if(this.board[i] === 0) {
+					this.cells[i].innerHTML = "";
 				} else {
-					this.cells[i].innerHTML = "O";
+					if (this.board[i] === player){
+						this.cells[i].innerHTML = "X";
+					} else {
+						this.cells[i].innerHTML = "O";
+					}
 				}
 			}
-		}
-	};
+		};
+		return {
+			reset: reset,
+			isEmptyCell: isEmptyCell,
+			writeCell: writeCell,
+			checkWin: checkWin,
+			checkTie: checkTie,
+			drawToken: drawToken
+		};
+	}();
+
 	//constructor for the Game class
 	var Game = function () {
 		this.board = new Board();
@@ -95,143 +108,163 @@
 		this.turn = 0;
 		this.state = null;
 		this.reset();
+
 	};
+
+
+	Game.prototype = function() {
 	//checks if a value is even or not
-	Game.prototype.isEven = function(value) {
-		return (value % 2 === 0) ? true : false;
-	};
+		isEven = function(value) {
+			return (value % 2 === 0) ? true : false;
+		},
 	//sends a message to a HTML container
-	Game.prototype.showMessage = function(info) {
-		var message = document.getElementById("message");
-		message.innerHTML = info;
-		//console.log(info);
-	};
+		showMessage = function(info) {
+			var message = document.getElementById("message");
+			message.innerHTML = info;
+			//console.log(info);
+		},
 	//First move for the cpu is random. Return number
-	Game.prototype.randomCell = function(min,max) {
+		randomCell = function(min,max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
-	};
+		},
 
 	//when starts a new game makes start either the cpu or    //player. this is for even games starts the cpu.
-	Game.prototype.reset = function() {
-		this.board.reset();
-		if(!this.isEven(this.turn)) {
-			this.state = waiting;
-			this.showMessage("Cpu turn");
-			this.board.writeCell(cpu, this.randomCell(0, 9));
+		reset = function() {
+			this.board.reset();
+			if(!this.isEven(this.turn)) {
+				this.state = waiting;
+				this.showMessage("Cpu turn");
+				this.board.writeCell(cpu, this.randomCell(0, 9));
 
-		}
-			this.turn++;
-			this.state = playing;
-			this.showMessage("Player turn");
-			this.board.drawToken();
+			}
+				this.turn++;
+				this.state = playing;
+				this.showMessage("Player turn");
+				this.board.drawToken();
 
-	};
+		},
  	//when a square is clicked this event is launch
-	Game.prototype.startMove = function(num) {
+		startMove = function(num) {
 
-		if (this.state === playing) {
+				if (this.state === playing) {
 
-			if (this.board.isEmptyCell(num)) {
+					if (this.board.isEmptyCell(num)) {
 
-				this.board.writeCell(1, num);
+						this.board.writeCell(1, num);
 
-				if (this.board.checkWin(player)) {
-					this.state = over;
-					this.showMessage("You Win");
+						if (this.board.checkWin(player)) {
+							this.state = over;
+							this.showMessage("You Win");
 
-				} else if (!this.board.checkTie()) {
-					this.state = over;
-					this.showMessage("A tie");
+						} else if (!this.board.checkTie()) {
+							this.state = over;
+							this.showMessage("A tie");
 
-				} else {
-					this.state = waiting;
-					this.showMessage("Cpu turn2");
-					//initiate the AI
-					this.findMove();
+						} else {
+							this.state = waiting;
+							this.showMessage("Cpu turn2");
+							//initiate the AI
+							this.findMove();
 
-					if(this.board.checkWin(cpu)) {
-						this.state = over;
-						this.showMessage("Cpu wins");
+							if(this.board.checkWin(cpu)) {
+								this.state = over;
+								this.showMessage("Cpu wins");
 
-					} else if (!this.board.checkTie()) {
+							} else if (!this.board.checkTie()) {
 
-						this.state = over;
+								this.state = over;
 
-						this.showMessage("A tie");
+								this.showMessage("A tie");
 
-					} else {
-						this.showMessage("Player turn");
+							} else {
+								this.showMessage("Player turn");
 
-						this.state = playing;
+								this.state = playing;
+							}
+						}
 					}
+					this.board.drawToken();
+
+				} else if (this.state == over) {
+							this.reset();
+						}
+		},
+
+		findMove = function () {
+			var bestMove = -100,
+				prevMove = -100,
+				pos = 0;
+			for (var i = 0; i < 9; i++) {
+				if (this.board.isEmptyCell(i)) {
+					this.board.writeCell(cpu, i);
+					prevMove = this.minMax();
+					if (prevMove > bestMove) {
+						bestMove = prevMove;
+						pos = i;
+					}
+					this.board.writeCell(0, i);
+				}
+
+			}
+			this.board.writeCell(cpu, pos);
+		},
+
+		minMax = function() {
+			var prevMove, bestMove = 100;
+
+			if (this.board.checkWin(cpu)) return 1;
+			if (!this.board.checkTie()) return 0 ;
+			for (var i = 0; i < 9; i++) {
+				if (this.board.isEmptyCell(i)) {
+					this.board.writeCell(player, i);
+					prevMove = this.maxMin();
+					if (prevMove < bestMove) {
+						bestMove = prevMove;
+					}
+					this.board.writeCell(0, i);
 				}
 			}
-			this.board.drawToken();
+			return bestMove;
 
-		} else if (this.state == over) {
-			   		this.reset();
-		}
-	};
+		},
 
-	Game.prototype.findMove = function () {
-		var bestMove = -100,
-			prevMove = -100,
-			pos = 0;
-		for (var i = 0; i < 9; i++) {
-			if (this.board.isEmptyCell(i)) {
-				this.board.writeCell(cpu, i);
-				prevMove = this.minMax();
-				if (prevMove > bestMove) {
-					bestMove = prevMove;
-					pos = i;
+		maxMin = function() {
+			var prevMove, bestMove = -100;
+
+			if (this.board.checkWin(player)) return -1;
+			if (!this.board.checkTie()) return 0 ;
+			for (var i = 0; i < 9; i++) {
+				if (this.board.isEmptyCell(i)) {
+					this.board.writeCell(cpu, i);
+					prevMove = this.minMax();
+					if (prevMove > bestMove) {
+						bestMove = prevMove;
+					}
+					this.board.writeCell(0, i);
 				}
-				this.board.writeCell(0, i);
 			}
+			return bestMove;
 
-		}
-		this.board.writeCell(cpu, pos);
-	};
+		};
 
-	Game.prototype.minMax = function() {
-		var prevMove, bestMove = 100;
+	return {
 
-		if (this.board.checkWin(cpu)) return 1;
-		if (!this.board.checkTie()) return 0 ;
-		for (var i = 0; i < 9; i++) {
-			if (this.board.isEmptyCell(i)) {
-				this.board.writeCell(player, i);
-				prevMove = this.maxMin();
-				if (prevMove < bestMove) {
-					bestMove = prevMove;
-				}
-				this.board.writeCell(0, i);
-			}
-		}
-		return bestMove;
+		isEven: isEven,
+		showMessage: showMessage,
+		randomCell: randomCell,
+		reset: reset,
+		startMove: startMove,
+		findMove: findMove,
+		minMax: minMax,
+		maxMin: maxMin
 
 	};
-
-	Game.prototype.maxMin = function() {
-		var prevMove, bestMove = -100;
-
-		if (this.board.checkWin(player)) return -1;
-		if (!this.board.checkTie()) return 0 ;
-		for (var i = 0; i < 9; i++) {
-			if (this.board.isEmptyCell(i)) {
-				this.board.writeCell(cpu, i);
-				prevMove = this.minMax();
-				if (prevMove > bestMove) {
-					bestMove = prevMove;
-				}
-				this.board.writeCell(0, i);
-			}
-		}
-		return bestMove;
-
-	};
+}();
 $(document).ready(function () {
 	'use strict';
-	var game = new Game();
+
+		var game = new Game();
+
 	$(".item").click(function() {
 		//console.log(parseInt(this.getAttribute("num")));
 
